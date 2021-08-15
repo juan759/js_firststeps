1c7a9e69d7d3b07a929a609a7e6acaed70ac2390
juan759 <jasso.antonio2001@gmail.com>
2021-08-15 11:33:59 -0500

    First scripts usin JS variables and the function console.log.

---
+++ first/primero.js
@@ -0,0 +1,3 @@
+
+console.log('Hola a todos')
+console.log(54309480928+6448)
---
+++ first/variables.js
@@ -0,0 +1,26 @@
+
+//esto es un comentario awevoooooooooooooooo
+var ju='este es un string'
+console.log(ju)
+console.log(typeof(ju))
+
+var hola=5974
+console.log(hola)
+console.log(typeof(hola))
+
+var nada=null
+console.log(nada)
+console.log(typeof(nada))
+
+var bol=false
+console.log(bol)
+console.log(typeof(bol))
+
+//Aquí estamos definiendo una variable, pero como no tiene asignado absolutamente
+//ningun valor su tipo es null.Y por lo tanto 
+var cola
+console.log(cola)
+console.log(typeof(cola))
+
+hola=hola+4987897
+console.log("este es el nuevo valor de hola:"+hola)
